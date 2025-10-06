b61f30aeeafcd0990c47c21f6c9aae5e9c2cc843
Bogdan Mircea <mirceapetrebogdan@gmail.com>
2025-10-07 02:36:38 +0300

    Add signal handling for graceful shutdown

--- rpi/src/main.rs
+++ rpi/src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 
 use dbus_cec::{CecDeviceType, CecDeviceTypeVec, CecLogicalAddress};
+use tokio::signal::{self, unix::SignalKind};
 use tracing_subscriber::EnvFilter;
 
 #[tokio::main(flavor = "current_thread")]
@@ -11,10 +12,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     tracing_subscriber::fmt().with_env_filter(filter).init();
 
-    dbus_cec::run(
+    let mut sigterm_handler = signal::unix::signal(SignalKind::terminate())?;
+    let mut sigint_handler = signal::unix::signal(SignalKind::interrupt())?;
+    let mut sigquit_handler = signal::unix::signal(SignalKind::quit())?;
+
+    let service = dbus_cec::run(
         "Raspberry Pi".to_owned(),
         CecDeviceTypeVec::new(CecDeviceType::PlaybackDevice),
         CecLogicalAddress::Tv,
-    )
-    .await
+    );
+
+    tokio::select! {
+        _ = sigterm_handler.recv() => Ok(()),
+        _ = sigint_handler.recv() => Ok(()),
+        _ = sigquit_handler.recv() => Ok(()),
+        res = service => res
+    }
 }
